4095a95cfdc17dffcd4d91b9489cb2068e90f3ad
Daniel Alvarez <alvarez17dani@gmail.com>
2020-01-30 13:15:17 +0100

    Add Drawer

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:preferencias_usuario_app/src/pages/settings-page.dart';
 
 class HomePage extends StatelessWidget {
 
@@ -10,6 +11,7 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Preferencias de usuario'),
       ),
+      drawer: _createDrawer(context),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
@@ -19,6 +21,49 @@ class HomePage extends StatelessWidget {
           Divider(),
           Text('Nombre Usuario'),
           Divider()
+        ],
+      ),
+    );
+  }
+
+  Drawer _createDrawer(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: <Widget>[
+          DrawerHeader(
+            child: Container(),
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/menu-img.jpg'),
+                fit: BoxFit.cover
+              )
+            ),
+          ),
+
+          ListTile(
+            leading: Icon(Icons.pages, color: Colors.blue),
+            title: Text('Home'),
+            onTap: () => Navigator.pushReplacementNamed(context, HomePage.routeName),
+          ),
+
+          ListTile(
+            leading: Icon(Icons.party_mode, color: Colors.blue),
+            title: Text('Party Mode'),
+            onTap: (){},
+          ),
+
+          ListTile(
+            leading: Icon(Icons.people, color: Colors.blue),
+            title: Text('People'),
+            onTap: (){},
+          ),
+
+          ListTile(
+            leading: Icon(Icons.settings, color: Colors.blue),
+            title: Text('Settings'),
+            onTap: () => Navigator.pushReplacementNamed(context, SettingsPage.routeName),
+          ),
         ],
       ),
     );
